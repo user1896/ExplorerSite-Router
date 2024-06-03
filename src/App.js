800c8d2998759ea04b45b62613e6fa6f50a55927
user1896 <abde.user15@gmail.com>
2024-06-03 05:36:34 +0200

    reduce initial load time

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from "./components/Home";
-import AboutUs from "./components/AboutUs";
 import Navbar from "./components/Navbar";
-import NotFound from './components/NotFound';
 import UserProfile from './components/UserProfile';
-import { useState } from 'react';
+import { useState, lazy, Suspense } from 'react';
+import Auth from "./components/Auth";
+import Login from "./components/Login";
+
+const AboutUs = lazy( () => import("./components/AboutUs") )
+const NotFound = lazy( () => import("./components/NotFound") )
 
 function App() {
   const [caller, setCaller] = useState("\'Home\'")
   const [current, setCurrent] = useState("\'Home\'")
 
+  const [username, setUsername] = useState("")
+  const [isLogged, setIsLogged] = useState(false)
+
   return (
-    <Routes>
-      <Route path='/'
-        element={<Navbar setCaller={setCaller} current={current} />}
-      >
-        <Route exact path='/'
-          element={
-            <Home caller={caller} setCurrent={setCurrent} />
-          }
-        />
-        <Route path='/about'
-          element={
-            <AboutUs caller={caller} setCurrent={setCurrent} />
-          }
-        />
-        <Route path='/users/:username' element={<UserProfile />} />
-      </Route>
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={() => <h1>Loading...</h1>} >
+      <Routes>
+        <Route path='/'
+          element={<Navbar setCaller={setCaller} current={current} />}
+        >
+          <Route exact path='/'
+            element={
+              <Home caller={caller} setCurrent={setCurrent} />
+            }
+          />
+          <Route path='/about'
+            element={
+              <AboutUs caller={caller} setCurrent={setCurrent} />
+            }
+          />
+          <Route path='/users/:username' element={<UserProfile />} />
+          <Route path='/login'
+            element={<Login setIsLogged={setIsLogged} setUsername={setUsername} />}
+          />
+          <Route path='/auth'
+            element={
+              isLogged ? <Auth username={username} /> : <Navigate replace to={"/login"} />
+            }
+          />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 }
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,5 @@
+export default function Login({setIsLogged, setUsername}){
+	return (
+		<>this is login</>
+	)
+}
